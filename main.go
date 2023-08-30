74e10c9aa10328b580ff802fa38d51fe9889e890
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-08-30 16:21:28 -0400

    Test graphql to get todo

--- main.go
+++ main.go
@@ -6,15 +6,36 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/graphql-go/graphql"
+	"github.com/mynhinguyentruong/gogo/schema"
 )
 
-func executeQuery(query string, schema graphql.Schema) {
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+  result := graphql.Do(graphql.Params{
+    Schema: schema,
+    RequestString: query,
+  })
+  
+  if result.HasErrors() {
+    fmt.Printf("Wrong result, unexpected error: %v", result.Errors)
+  }
+
+  return result
 } 
 
 func main() {
   router := gin.Default()
 
   router.GET("/greet", Greeting)
+  router.GET("/list", func (c *gin.Context) {
+    list := schema.InitTodoList()
+    c.IndentedJSON(http.StatusOK, list)
+  })
+
+  router.GET("/graphql", func (c *gin.Context) {
+    // http://localhost:8080/graphql?query={todo(id:"a"){id, text}}
+    result := executeQuery(c.Query("query"), schema.TodoSchema)
+    c.IndentedJSON(http.StatusOK, result)
+  })
 
   router.Run()
 }
--- schema/schema.go
+++ schema/schema.go
@@ -15,7 +15,9 @@ type Todo struct {
 
 type TodoList []Todo
 
-func initTodoList() TodoList {
+var list TodoList = InitTodoList()
+
+func InitTodoList() TodoList {
   var list TodoList
 
   todo1 := Todo{"a", "asdasdad", false}
@@ -28,27 +30,45 @@ func initTodoList() TodoList {
   return list
 }
 
+var todoType = graphql.NewObject(graphql.ObjectConfig{
+  Name: "Todo",
+  Fields: graphql.Fields{
+    "id": &graphql.Field{
+      Type: graphql.String,
+    },
+    "text": &graphql.Field{
+      Type: graphql.String,
+    },
+    "completed": &graphql.Field{
+      Type: graphql.Boolean,
+    },
+  },
+})
+
 var rootQuery = graphql.NewObject(graphql.ObjectConfig{
   Name: "RootQuery",
-  Fields: graphql.Field{
-    Type: todoType,
-    Description: "Get a single todo",
-    Args: graphql.FieldConfigArgument{
-      "id": &graphql.ArgumentConfig{
-        Type: graphql.String,
-      },
-    },
-    Resolve: func (params graphql.ResolveParams) (interface{}, error) {
-      idQuery, isOk := params.Args["id"].(string)
-      if isOk {
-        for _, todo := range TodoList {
-          if todo.id == idQuery {
-            return todo, nil
+  Fields: graphql.Fields{
+      "todo": &graphql.Field{
+        Type: todoType,
+        Description: "Get a single todo",
+        Args: graphql.FieldConfigArgument{
+          "id": &graphql.ArgumentConfig{
+            Type: graphql.String,
+          },
+        },
+        Resolve: func (params graphql.ResolveParams) (interface{}, error) {
+          idQuery, isOk := params.Args["id"].(string)
+          
+          if isOk {
+            for _, todo := range list {
+              if todo.ID == idQuery {
+                return todo, nil
+              }
+            }
           }
-        }
-      }
 
-      return Todo{}, errors.New("Todo does not exist")
+          return Todo{}, errors.New("Todo does not exist")
+      },
     },
   },
 })
