9e845fe047075372514c4099da88cb3418673bf8
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-08-30 11:40:24 -0400

    Add and try testing

---
+++ greeting_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestGreetingHello(t *testing.T) {
+  hello := GreetingHello()
+
+  if hello != "Hello" {
+    t.Errorf("It did not greet Hello but %s instead", hello)
+  }
+} 
--- main.go
+++ main.go
@@ -1,3 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
 func main() {
-  fmt.Println("Hi")  
+  router := gin.Default()
+
+  router.GET("/greet", Greeting)
+  
+  router.Run()
+}
+
+func Greeting(c *gin.Context) {
+  fmt.Println("Greeting: ")
+  c.IndentedJSON(http.StatusOK, "hi")
 }
